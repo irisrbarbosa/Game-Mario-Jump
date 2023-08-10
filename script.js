7945b5f49df01506ded2ab312c574394f449c110
Tamiris Rodrigues Barbosa <95186331+irisrbarbosa@users.noreply.github.com>
2023-08-10 14:56:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
-const jump = () => {
-  mario.classList.add('jump');
+    const jump = () => {
+        mario.classList.add('jump');
 
-setTimeout(() => {
+        setTimeout(() => {
 
-mario.classList.remove('jump');
-}, 500);
+            mario.classList.remove('jump');
 
-}
+        }, 500)
+    }
 
-const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-  console.log(marioPosition);
-  if (pipePosition ≤ 120 && pipePosition > 0 && marioPosition < 80) {
-    
-    pipe.style.animation ='none';
-    pipe.style.left ='${pipePosition}px';
-    
-    mario.style.animation ='none';
-    mario.style.bottom ='${marioPosition}px';
-    
-    mario.src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3Dwap29ECO9rI82NVMF-9ZRNXQDzqGM8TcA&usqp=CAU'
-    mario.style.width = '75px'
-    mario.style.marginLeft = '50px'
-    
-    clearInterval('loop');
-  }
-}, 10);
+const loop = setInterval(() => {
 
-const loop = setInterval(()=> {
-  
-  console.log('loop')
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-document.addEventListener('touchstart', jump);
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation ='none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation ='none';
+        mario.style.bottom = `${pipePosition}px`;
+
+        mario.src ='./Imagens/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+    }
+}, 10)
+
+
+document.addEventListener('keydown', jump);
